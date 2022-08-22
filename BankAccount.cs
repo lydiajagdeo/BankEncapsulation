a958294def1c842bc8d12be1b1618c9921da0734
“Lydia <lydiajagdeo@gmail.com>
2022-08-22 10:33:48 -0400

    BankEncapsulation initial

---
+++ BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        
+           private double _balance = 0;
+
+           public void Deposit( double money)
+           { 
+            _balance += money;
+            
+           }   
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+        
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using BankEncapsulation;
+
+BankAccount myAccount = new BankAccount();
+
+Console.WriteLine("Enter amount to be deposited");
+
+var amountToDeposit = double.Parse(Console.ReadLine());
+
+myAccount.Deposit(amountToDeposit);
+
+myAccount.Deposit(200.00);
+
+Console.WriteLine(myAccount.GetBalance());
